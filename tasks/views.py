747f4f7d8a49f97fde0808516caaa33272cb50d8
Azad Hossan Munna <azadhossanmunna1209@gmail.com>
2025-06-26 00:30:40 +0600

    Dynamic url implemented

--- tasks/views.py
+++ tasks/views.py
@@ -8,3 +8,7 @@ def contact(request):
         return HttpResponse("this is my contact list")
 def show_task(request):
       return HttpResponse("this is what it is")
+def show_specific_task(request,id):
+      print("id",id)
+      print("id type",type(id))
+      return HttpResponse("This is specific task page")
