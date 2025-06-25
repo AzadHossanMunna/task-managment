0bb483e0217d80e8dd9a79cdd49171056083548d
Azad Hossan Munna <azadhossanmunna1209@gmail.com>
2025-06-25 06:58:44 +0600

    Intial Project Done

---
+++ tasks/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def home(request):
+    return HttpResponse("welcome to the task management system")
+    
+def contact(request):
+        return HttpResponse("this is my contact list")
+def show_task(request):
+      return HttpResponse("this is what it is")
